44c072f218b9f59853aa2afd7808a89a45f04cb2
Huu Hoang <hoangduchuuvn@gmail.com>
2017-07-07 00:07:00 +0700

    firebase Authenticate

--- app/src/main/java/net/hoangduchuu/foody/MainActivity.java
+++ app/src/main/java/net/hoangduchuu/foody/MainActivity.java
@@ -1,10 +1,17 @@
 package net.hoangduchuu.foody;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,77 +22,52 @@ import com.google.firebase.database.Query;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ChildEventListener {
-    ListView listView;
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
 
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-    List<User> userList;
-    AdapterUser adapter;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference().child("users");
-        listView = (ListView) findViewById(R.id.lvView);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        userList = new ArrayList<>();
-
-
-        adapter = new AdapterUser(getApplicationContext(), R.layout.item_user_row, userList);
-        listView.setAdapter(adapter);
-
-        Query query = databaseReference.orderByKey();
-
-        query.addChildEventListener(this);
-
+        mAuth.createUserWithEmailAndPassword("huu@1dreamm.com", "123456789").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("kiemtra", "tao thanh cong");
+                } else {
+                    Log.d("kiemtra", "tao khong thanh cong" + task.getException().getMessage().toString());
+                }
+            }
+        });
 
     }
 
 
     @Override
-    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-        User user = dataSnapshot.getValue(User.class);
-        userList.add(user);
-        adapter.notifyDataSetChanged();
-        Log.d("kiemtra", "added");
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Log.d("kiemtra", firebaseAuth.getCurrentUser().getEmail());
+
+        }else {
+            Toast.makeText(this, "chua ra gi", Toast.LENGTH_SHORT).show();
+        }
+        
     }
 
     @Override
-    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-        Log.d("kiemtra", "onChildChanged");
-        refreshList();
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(this);
     }
 
     @Override
-    public void onChildRemoved(DataSnapshot dataSnapshot) {
-        Log.d("kiemtra", "onChildRemoved");
-        refreshList();
-
-
-    }
-
-    @Override
-    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-        Log.d("kiemtra", "onChildMoved");
-        refreshList();
-
-    }
-
-    @Override
-    public void onCancelled(DatabaseError databaseError) {
-        Log.d("kiemtra", "onCancelled");
-        refreshList();
-
-
-    }
-
-    private void refreshList() {
-        userList.clear();
-        databaseReference.removeEventListener(this);
-        databaseReference.addChildEventListener(this);
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(this);
     }
 }
