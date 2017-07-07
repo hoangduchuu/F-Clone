7ee925267dd88f64a5f8a57d4f3821ae68abeabe
Huu Hoang <hoangduchuuvn@gmail.com>
2017-07-07 10:14:46 +0700

    google signin ok

--- app/src/main/java/net/hoangduchuu/foody/MainActivity.java
+++ app/src/main/java/net/hoangduchuu/foody/MainActivity.java
@@ -11,11 +11,20 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,80 +35,79 @@ import com.google.firebase.database.Query;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener {
+public class MainActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener, GoogleApiClient.OnConnectionFailedListener {
 
     FirebaseAuth mAuth;
-    EditText edtMail;
-    EditText edtPass;
-    Button btnDangNhap;
+    SignInButton btnGoogleSignin;
+    final int MY_REQUEST_CODE = 333;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnGoogleSignin = (SignInButton) findViewById(R.id.btnGoogleLogin);
+
+
         mAuth = FirebaseAuth.getInstance();
-        edtMail = (EditText) findViewById(R.id.edtMail);
-        edtPass = (EditText) findViewById(R.id.edtPass);
-        btnDangNhap = (Button) findViewById(R.id.btnDangNhap);
-//        mAuth.signOut();
+        mAuth.signOut();
 
-
-//        create();
-        login();
-
+        googleSigninMethod();
     }
 
-    private void login() {
+    private void googleSigninMethod() {
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestProfile()
+                .build();
 
-        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+        final GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        btnGoogleSignin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String mail = edtMail.getText().toString().trim();
-                String pass = edtPass.getText().toString().trim();
+                Intent i = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(i, MY_REQUEST_CODE);
 
-                mAuth.signInWithEmailAndPassword(mail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                String tokenID = result.getSignInAccount().getIdToken();
+                AuthCredential credential = GoogleAuthProvider.getCredential(tokenID, null);
+                mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Log.d("kiemtra", "dang nhap thanh cong");
+                            Log.d("kiemtra - gooogle - OK", "dang nhap thanh cong gooogle oi");
                         } else {
-                            Log.d("kiemtra", "dang nhap that bai");
+                            Log.d("kiemtra - gooogle - Failed", "dang nhap failed gooogle oi");
 
                         }
                     }
-
-
                 });
+
             }
-        });
+        }
     }
-
-    private void gotoUpdateUserActivity() {
-        startActivity(new Intent(MainActivity.this, UpdateUserActivity.class));
-    }
-
-    private void create() {
-        mAuth.createUserWithEmailAndPassword("hoangduchuuvn@gmail.com", "123456789").addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d("kiemtra", "tao thanh cong");
-                } else {
-                    Log.d("kiemtra", "tao khong thanh cong" + task.getException().getMessage().toString());
-                }
-            }
-        });
-
-    }
-
 
     @Override
     public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
         FirebaseUser user = mAuth.getCurrentUser();
         if (user != null) {
             Log.d("kiemtra", firebaseAuth.getCurrentUser().getEmail());
-            gotoUpdateUserActivity();
 
         } else {
             Toast.makeText(this, "dang nhap that bai", Toast.LENGTH_SHORT).show();
@@ -117,5 +125,10 @@ public class MainActivity extends AppCompatActivity implements FirebaseAuth.Auth
     protected void onStop() {
         super.onStop();
         mAuth.removeAuthStateListener(this);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
