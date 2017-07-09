332de377b861027396bc94b40232df3355b7a40f
Huu Hoang <hoangduchuuvn@gmail.com>
2017-07-09 09:19:47 +0700

    smoth scroll fragment  when click radio button

--- app/src/main/java/net/hoangduchuu/foody/View/HomePageActivity.java
+++ app/src/main/java/net/hoangduchuu/foody/View/HomePageActivity.java
@@ -1,10 +1,12 @@
 package net.hoangduchuu.foody.View;
 
+import android.support.annotation.IdRes;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,10 +17,11 @@ import net.hoangduchuu.foody.R;
 
 import static net.hoangduchuu.foody.R.id.viewPagerHomePage;
 
-public class HomePageActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class HomePageActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, RadioGroup.OnCheckedChangeListener {
     FirebaseAuth mAuth;
     ViewPager viewPagerHome;
     RadioButton rd_oDau, rd_anGi;
+    RadioGroup radioGroupHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,8 @@ public class HomePageActivity extends AppCompatActivity implements ViewPager.OnP
         rd_anGi = (RadioButton) findViewById(R.id.rd_angi);
         rd_oDau = (RadioButton) findViewById(R.id.rd_odau);
 
+        radioGroupHome = (RadioGroup) findViewById(R.id.radioGroupHome);
+        radioGroupHome.setOnCheckedChangeListener(this);
 
 
     }
@@ -60,5 +65,17 @@ public class HomePageActivity extends AppCompatActivity implements ViewPager.OnP
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
+        switch (i) {
+            case R.id.rd_odau:
+                viewPagerHome.setCurrentItem(0,true);
+                break;
+            case R.id.rd_angi:
+                viewPagerHome.setCurrentItem(1,true);
+                break;
+        }
     }
 }
